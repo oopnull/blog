79a2dcba1298b776f003c2f35c90ce6e3b19a9c2
oopnull <1056096898@qq.com>
2024-05-18 19:07:52 +0800

    add router instance

---
+++ blog-ui/src/router/index.tsx
@@ -0,0 +1,17 @@
+// config routers
+import Login from "../pages/Login";
+import Layout from "../pages/Layout";
+import { createBrowserRouter } from "react-router-dom";
+
+// config router instance
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
+export default router;
