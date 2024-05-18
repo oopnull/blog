8028b39a7123cff2e0e962fedefc22c2bb95d5a2
oopnull <1056096898@qq.com>
2024-05-18 23:06:11 +0800

    add login

--- blog-ui/src/pages/Login/index.tsx
+++ blog-ui/src/pages/Login/index.tsx
@@ -1,5 +1,42 @@
+import Password from "antd/es/input/Password";
+import "./index.scss";
+import { Card, Form, Input, Button } from "antd";
+
 const Login = () => {
-  return <div>This is Login</div>;
+  const onFinish = (values: Event) => {
+    console.log(values);
+  };
+  return (
+    <div className="login">
+      <Card className="login-container">
+        <Form onFinish={onFinish} validateTrigger="onBlur">
+          <Form.Item
+            name="username"
+            rules={[
+              { required: true, message: "Please input your username" },
+              {
+                pattern: /^1[3-9]\d{9}$/,
+                message: "Please input correct format of username",
+              },
+            ]}
+          >
+            <Input size="large" placeholder="username"></Input>
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: "Please input your password" }]}
+          >
+            <Input size="large" placeholder="password"></Input>
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large" block>
+              Login
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  );
 };
 
 export default Login;
