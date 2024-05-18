05f2e2740d17562e9a00a8db4f6782709ddf2e76
oopnull <1056096898@qq.com>
2024-05-18 23:23:21 +0800

    add axios requet&response

--- blog-ui/README.md
+++ blog-ui/README.md
@@ -39,7 +39,19 @@ Instead, it will copy all the configuration files and the transitive dependencie
 
 You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
 
-## Learn More
+## Tech-Docs
+
+Router
+https://reactrouter.com/en/main/start/tutorial
+
+Ant-design
+https://ant.design/docs/react/use-with-create-react-app-cn
+
+Craco
+https://craco.js.org/docs/
+
+Axios
+https://axios-http.com/docs/intro
 
 You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
 
---
+++ blog-ui/src/utils/request.js
@@ -0,0 +1,35 @@
+// handle global request axios
+import axios from "axios";
+
+const request = axios.create({
+  baseURL: "http://geek.itheima.net/v1_0",
+  timeout: 5000,
+});
+
+// Add a request interceptor
+request.interceptors.request.use(
+  (config) => {
+    // Do something before request is sent
+    return config;
+  },
+  (error) => {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
+
+// Add a response interceptor
+request.interceptors.response.use(
+  (response) => {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response;
+  },
+  (error) => {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error);
+  }
+);
+
+export { request };
