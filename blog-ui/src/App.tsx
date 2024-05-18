2db6ddac672e56bb33dfff8f896a4dc5951c526a
oopnull <1056096898@qq.com>
2024-05-18 18:37:06 +0800

    install scss & antd

--- blog-ui/src/App.tsx
+++ blog-ui/src/App.tsx
@@ -1,26 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button } from "antd";
 
-function App() {
+function APP() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      This is react<Button type="primary">test</Button>
     </div>
   );
 }
-
-export default App;
+export default APP;
